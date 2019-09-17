173ab9d3fecbe9fa7a40381df776fdc621b0f87b
Sharfuzzamaan <msharfuzzaman9@gmail.com>
2019-09-17 12:35:51 +0600

    data store in blog

--- app/Http/Controllers/BlogsController.php
+++ app/Http/Controllers/BlogsController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 use App\Blog;
-// use Illuminate\Http\Request;
+use Illuminate\Http\Request; 
 
 class BlogsController extends Controller
 {
     public function index(){
         $blogs = Blog::all();
         return view('blogs.index', compact('blogs'));
+    }
+
+    public function create(){
+        return view('blogs.create');
+    }
+
+    public function store(Request $request){
+        $input = $request->all();
+        $blog = Blog::create($input);
+        // $blog = new Blog();
+        // $blog->title = $request->title;
+        // $blog->body = $request->body;
+        // $blog->save();
+        return redirect('/blogs');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/blogs', 'BlogsController@index');
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/blogs', 'BlogsController@index');
+Route::get('/blogs/create', 'BlogsController@create');
+Route::post('/blogs/store', 'BlogsController@store')->name('blogs.store');
